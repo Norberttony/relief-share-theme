0dadf0e9fa67c5436f568a7eeaaee90039bd633b
Norberttony <norberttony@icloud.com>
2025-02-17 15:37:35 -0500

    fixed sidebar

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -6,6 +6,8 @@
  */
 ?>
 
-<aside id = "primary-sidebar" class = "primary-sidebar widget-area" role = "complementary">
-    <?php dynamic_sidebar('sidebar'); ?>
-</aside>
+<?php if (is_active_sidebar('sidebar')){ ?>
+    <aside id = "primary-sidebar" class = "primary-sidebar widget-area" role = "complementary">
+        <?php dynamic_sidebar('sidebar'); ?>
+    </aside>
+<?php }
