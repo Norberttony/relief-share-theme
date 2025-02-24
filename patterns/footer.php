a960326f3dec6c34848358fdf279daa23560de31
Norberttony <norberttony@icloud.com>
2025-02-23 21:13:10 -0500

    fixed gap issues, added color palette, fixed footer block

--- patterns/footer.php
+++ patterns/footer.php
@@ -5,15 +5,13 @@
  * Categories: footer
  */
 ?>
-
-<footer>
-    <?php
-        wp_nav_menu(array(
-            'theme_location' => 'footer_menu',
-            'container_class' => 'footer_menu'
-        ));
-    ?>
-    Copyright © 2024 Relief Share Florida, a 501(c)(3) non-profit organization, All Rights Reserved.
-</footer>
-
-<?php wp_footer(); ?>
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|40"}},"backgroundColor":"contrast","textColor":"base"} -->
+<div class="wp-block-group">
+    <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
+    <!-- wp:paragraph {"textAlign":"center"} -->
+    <p class="has-text-align-center">
+        Copyright © 2024 Relief Share Florida, a 501(c)(3) non-profit organization, All Rights Reserved.
+    </p>
+    <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
