14aa09cccf9c7cffe608c4553acae4c5c0b1a987
Norberttony <norberttony@icloud.com>
2025-02-26 20:32:00 -0500

    separated header into masthead and navigation part

--- patterns/header.php
+++ patterns/header.php
@@ -5,14 +5,6 @@
  * Categories: header
  */
 ?>
-
-<header class = "header">
-    <div class = "header__logo-container">
-        <a href = "<?php echo esc_url(home_url('/'));?>">
-            <img class = "header__logo" src = "<?php echo get_parent_theme_file_uri('/assets/images/big-logo.png');?>" alt = "Site Logo">
-        </a>
-    </div>
-</header>
 <div id = "header-menu-container">
     <div class = "header-menu__mobile-button"></div>
     <img src = "<?php echo get_parent_theme_file_uri('/assets/images/big-square-logo.png'); ?>" class = "header__small-logo">
---
+++ patterns/masthead.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Masthead
+ * Slug: relief-share/masthead
+ * Categories: header
+ */
+?>
+<header class = "header">
+    <div class = "header__logo-container">
+        <a href = "<?php echo esc_url(home_url('/'));?>">
+            <img class = "header__logo" src = "<?php echo get_parent_theme_file_uri('/assets/images/big-logo.png');?>" alt = "Site Logo">
+        </a>
+    </div>
+</header>
