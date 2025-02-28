19754cf0c4dd214d99fbef339e93ba6ef15a170e
Norberttony <norberttony@icloud.com>
2025-02-27 19:59:06 -0500

    made sidebar editable in the block editor

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -5,9 +5,3 @@
  * Categories: sidebar
  */
 ?>
-
-<?php if (is_active_sidebar('sidebar')){ ?>
-    <aside id = "primary-sidebar" class = "primary-sidebar widget-area" role = "complementary">
-        <?php dynamic_sidebar('sidebar'); ?>
-    </aside>
-<?php }
