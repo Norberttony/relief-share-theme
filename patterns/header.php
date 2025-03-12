d07067eb9527abb3f407bcb6576d1d652510ade2
Norberttony <norberttony@icloud.com>
2025-03-12 18:47:16 -0400

    modified templates to include menu w/ position sticky

--- patterns/header.php
+++ patterns/header.php
@@ -5,7 +5,7 @@
  * Categories: header
  */
 ?>
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"},"className":"menu-container"} -->
 <div class="wp-block-group">
     <!-- wp:image {"id":127,"width":"70px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
     <figure class="wp-block-image size-full is-resized">
