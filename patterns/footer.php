95e33eab0d5d4f1d704100b347b9856c11a9f899
Norberttony <norberttony@icloud.com>
2025-03-13 10:14:04 -0400

    style changes for footer and headings

--- patterns/footer.php
+++ patterns/footer.php
@@ -5,9 +5,9 @@
  * Categories: footer
  */
 ?>
-<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|40"}},"backgroundColor":"contrast","textColor":"base"} -->
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|80"},"blockGap":"var:preset|spacing|50","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"contrast","textColor":"base"} -->
 <div class="wp-block-group">
-    <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"}} /-->
+    <!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center","flexWrap":"wrap"},"className":"footer-menu"} /-->
     <!-- wp:paragraph {"textAlign":"center"} -->
     <p class="has-text-align-center">
         Copyright © 2024 Relief Share Florida, a 501(c)(3) non-profit organization, All Rights Reserved.
