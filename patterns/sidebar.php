4daa5d088b45dd53cd6b9d05d32f7a1e30de12ec
Norberttony <norberttony@icloud.com>
2025-03-18 10:33:00 -0400

    style and sidebar changes

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -5,3 +5,9 @@
  * Categories: sidebar
  */
 ?>
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+
+</div>
+<!-- /wp:group -->
