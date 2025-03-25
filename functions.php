4122437bde8e34049bcf0e0764158536c7b95e41
Norberttony <norberttony@icloud.com>
2025-03-24 21:18:27 -0400

    deleted unneeded functions, made carousel buttons bigger, added screenshot

--- functions.php
+++ functions.php
@@ -1,33 +1,6 @@
 <?php
 /* This file allows theme customizability without modifying PHP code */
 
-// main menu navigation
-function relief_share_add_menus(){
-    register_nav_menus(array(
-        'header-menu' => __('Header Menu'),
-        'footer_menu' => __('Footer Menu')
-    ));
-}
-
-add_action('init', 'relief_share_add_menus');
-
-
-// sidebar
-add_filter('default_wp_template_part_areas', 'relief_share_template_part_areas');
-
-function relief_share_template_part_areas(array $areas) {
-	$areas[] = array(
-		'area'        => 'sidebar',
-		'area_tag'    => 'aside',
-		'label'       => __('Sidebar', 'relief-share'),
-		'description' => __('Sidebars appear alongside the content.', 'relief-share'),
-		'icon'        => 'sidebar'
-	);
-
-	return $areas;
-}
-
-
 // styles
 function relief_share_enqueue_styles(){
     wp_enqueue_style('relief-share-style', get_stylesheet_uri());
