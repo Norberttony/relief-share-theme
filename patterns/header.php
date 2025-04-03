d26d4afe8b39f72d1cb925ddcc85ff3e06d6b74f
Norberttony <norberttony@icloud.com>
2025-04-03 08:36:39 -0400

    blockgap fixes

--- patterns/header.php
+++ patterns/header.php
@@ -5,7 +5,7 @@
  * Categories: header
  */
 ?>
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
 <div class="wp-block-group">
     <!-- wp:image {"id":127,"width":"70px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
     <figure class="wp-block-image size-full is-resized">
@@ -15,7 +15,7 @@
 
     <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
     <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-        <!-- wp:navigation {"ref":139,"className":"header-menu","layout":{"type":"flex","justifyContent":"center"}} /-->
+        <!-- wp:navigation {"ref":139,"className":"header-menu","style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","justifyContent":"center"}} /-->
     </div>
     <!-- /wp:group -->
 </div>
