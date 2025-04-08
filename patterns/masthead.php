c0175bdb52b87c0958d53f72070cdceab6b1ffb9
Norberttony <norberttony@icloud.com>
2025-04-07 22:08:44 -0400

    change masthead design, fixed mobile styles

--- patterns/masthead.php
+++ patterns/masthead.php
@@ -5,10 +5,10 @@
  * Categories: header
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-    <!-- wp:image {"width":"643px","height":"auto","align":"center","className":"masthead-figure","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|60","top":"0"}}}} -->
-    <figure class="wp-block-image aligncenter is-resized masthead-figure" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--60)">
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)">
+    <!-- wp:image {"width":"643px","height":"auto","align":"center","className":"masthead-figure","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40","top":"var:preset|spacing|40","right":"0","left":"0"}}}} -->
+    <figure class="wp-block-image aligncenter is-resized masthead-figure" style="margin-top:var(--wp--preset--spacing--40);margin-right:0;margin-bottom:var(--wp--preset--spacing--40);margin-left:0">
         <img src="http://relief-shareinternational.org/wp-content/themes/assets/assets/images/big-logo.png" alt="Site Logo" style="width:643px;height:auto"/>
     </figure>
     <!-- /wp:image -->
